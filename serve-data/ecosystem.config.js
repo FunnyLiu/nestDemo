83cf8d724d3c52ed2ed18eeb1a7f7fa80f9fddb8
刘放 <362512489@qq.com>
2019-05-14 10:58:38 +0800

    refactor: change pm2 config

--- serve-data/ecosystem.config.js
+++ serve-data/ecosystem.config.js
@@ -10,6 +10,9 @@ module.exports = {
     watch: false,
     max_memory_restart: '1G',
     env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
       NODE_ENV: 'production'
     }
   }],
