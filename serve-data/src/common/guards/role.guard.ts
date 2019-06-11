0fa31d7bf1f3cec4216829ae7061a77d2e0e070b
刘放 <362512489@qq.com>
2019-06-11 09:25:23 +0800

    fix: token not exists

--- serve-data/src/common/guards/role.guard.ts
+++ serve-data/src/common/guards/role.guard.ts
@@ -3,6 +3,7 @@ import * as jwt from 'jsonwebtoken';
 
 import { Reflector } from '@nestjs/core';
 import { SECRET } from '../constants/secret';
+import { ForbiddenException } from '../exceptions/forbidden.exception';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -15,6 +16,9 @@ export class RolesGuard implements CanActivate {
         }
         const request = context.switchToHttp().getRequest();
         const authorization = request.header('Authorization')
+        if(!authorization){
+            throw new ForbiddenException('you have no auth')
+        }
         const token = (authorization as string).split(' ')[1];
         const decoded: any = jwt.verify(token, SECRET);
         const hasRole = () => decoded.roles.some((role) => 
